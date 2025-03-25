198e49f877d954e7b70a072f1e1f6186039ec7fb
prafessor <olegmilyo@gmail.com>
2025-03-25 17:17:33 +0200

    do hw4 of react

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,25 @@
+import { FaHeart } from 'react-icons/fa6';
+import css from './ImageCard.module.css';
+
+export default function ImageCard({ picture }) {
+  console.log(picture);
+  return (
+    <>
+      <div className={css.img_wrapper}>
+        <img src={picture.urls.small} alt={picture.alt_description} />
+      </div>
+
+      <div className={css.description_wrapper}>
+        <div className={css.title_wrapper}>
+          <h3 className={css.title}>{picture.alt_description}</h3>
+          <span class={css.name}>{picture.user.name}</span>
+        </div>
+        
+        <span className={css.static}>
+          <FaHeart className={css.icon} />
+          {picture.likes}
+        </span>
+      </div>
+    </>
+  );
+}
